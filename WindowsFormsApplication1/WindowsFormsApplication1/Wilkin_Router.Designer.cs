b1490dbc0a2598a43a0f07675ecb82b2f446f094
unknown <tandoni@TANDONI-W530G2.rose-hulman.edu>
2015-05-15 21:01:49 -0400

    first commit. setup

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Wilkin_Router.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Wilkin_Router
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Wilkin_Router
+            // 
+            this.ClientSize = new System.Drawing.Size(909, 572);
+            this.Name = "Wilkin_Router";
+            this.Load += new System.EventHandler(this.Wilkin_Router_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Wilkin_Router.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Wilkin_Router : Form
+    {
+        public Wilkin_Router()
+        {
+            InitializeComponent();
+        }
+
+        private void Wilkin_Router_Load(object sender, EventArgs e)
+        {
+            Form f1 = new Form();
+            f1.Text = "Wilkin ROUTER";
+        }
+    }
+}
